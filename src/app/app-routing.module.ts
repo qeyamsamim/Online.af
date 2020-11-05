9a9b6e052139bffde1a9aa86a362f455cfb0fb68
Ahmad Samim Qeyam <qeyam_a@auca.kg>
2020-11-05 16:00:06 +0430

    Added Routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CartComponent } from './components/cart/cart.component';
+import { HomeComponent } from './components/home/home.component';
+import { FoodComponent } from './components/food/food.component';
+import { StoreComponent } from './components/store/store.component';
+import { PharmacyComponent } from './components/pharmacy/pharmacy.component';
+import { ElectronicsComponent } from './components/electronics/electronics.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'food', component: FoodComponent },
+  { path: 'store', component: StoreComponent },
+  { path: 'pharmacy', component: PharmacyComponent },
+  { path: 'electronics', component: ElectronicsComponent },
+  { path: 'profile', component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,34 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CartComponent } from './components/cart/cart.component';
+import { TabComponent } from './components/tab/tab.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { HomeComponent } from './components/home/home.component';
+import { FoodComponent } from './components/food/food.component';
+import { StoreComponent } from './components/store/store.component';
+import { PharmacyComponent } from './components/pharmacy/pharmacy.component';
+import { ElectronicsComponent } from './components/electronics/electronics.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CartComponent,
+    TabComponent,
+    HomeComponent,
+    FoodComponent,
+    StoreComponent,
+    PharmacyComponent,
+    ElectronicsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +42,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatSidenavModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
